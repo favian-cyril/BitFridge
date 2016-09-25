9cca69a55a3bc836d5d734f3ea59415304dad385
chadmadna <chadmadna@gmail.com>
2016-09-25 18:16:14 +0700

    Modified get() to use else

--- app/models/apicalls.js
+++ app/models/apicalls.js
@@ -1,5 +1,5 @@
 var request = require('request')
-var append = require ('append-query')
+var append = require('append-query')
 
 var SPOONACULAR_API_KEY = 'glMwvNQWGMmsh9O1FUmZDPp6sJ67p1aNuApjsn8FLnNij2cDCx'
 
@@ -16,14 +16,14 @@ function searchIngredients (string, autocomplete, cb) {
     number: n,
     query: string
   }
-  _get(url, params, cb)
+  get(url, params, cb)
 }
 
 /**
  * Wrapper GET function using 'request' library
  * Callback receives (err, res, body)
  */
-function _get (url, params, cb) {
+function get (url, params, cb) {
   var options = {
     url: append(url, params),
     headers: { 'X-Mashape-Key': SPOONACULAR_API_KEY }
@@ -32,25 +32,11 @@ function _get (url, params, cb) {
     .get(options, function (err, res, body) {
       if (!err && res.statusCode == 200)
         cb(null, res, JSON.parse(body))
-    })
-    .on('error', function (err) {
-      cb(err)
+      else {
+        cb(err)
+      }
     })
 }
 
-/**
- * Wrapper POST function using 'request' library
- * Callback receives (err, res, body)
- */
-function _post (options, cb) {
-  request
-    .get(options, function (err, res, body) {
-      if (!err && res.statusCode == 200)
-        cb(null, res, body)
-    })
-    .on('error', function (err) {
-      cb(err)
-    })
-}
 
 module.exports = searchIngredients
