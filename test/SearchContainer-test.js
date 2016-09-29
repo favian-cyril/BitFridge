1d047a364dfed3d1f20cb37faf381792d93af646
favial-cyril <favian.cyril@gmail.com>
2016-09-29 11:44:49 +0700

    Finished test modules for sea-01

--- test/SearchContainer-test.js
+++ test/SearchContainer-test.js
@@ -1,10 +1,14 @@
-import {mock} from 'simple-mock'
+import sinon from 'sinon'
 import {mount, shallow} from 'enzyme'
 import {assert} from 'chai'
 import React from 'react'
 import SearchContainer from '../app/containers/SearchContainer'
 import SuggestionsList from '../app/components/SuggestionsList'
+import IngredientSuggestion from '../app/components/IngredientSuggestion'
 import SearchBar from '../app/components/SearchBar'
+import Preloader from '../app/components/Preloader'
+import ErrorMsg from '../app/components/ErrorMsg'
+import {searchIngredients} from '../app/models/apicalls'
 
 import jsdom from 'jsdom'
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
@@ -35,5 +39,62 @@ describe('SearchBar', function() {
   it('should have an input form', function() {
     var wrapper = shallow(<SearchBar />)
     assert.equal(wrapper.find('input').length, 1)
+  })
+})
+describe('SuggestionsList', function() {
+  this.slow(500)
+  it('should show SuggestionsList when parsed a text', function() {
+    var input = 'foo'
+    var mock = sinon.stub(searchIngredients).returns(null, 200, input)
+    var wrapper = mount(<SuggestionsList />)
+    wrapper.setProps({
+      searchText: input,
+      isFocused: true
+      })
+    function assertTest() {
+      mock.restore()
+      assert.equal(wrapper.find(IngredientSuggestion).length, 1)
+    }
+    setTimeout(assertTest, 10)
+  })
+  it('should show Preloader', function() {
+    var input = 'foo'
+    var mock = sinon.stub(searchIngredients).returns(null, 200, input)
+    var wrapper = mount(<SuggestionsList />)
+    wrapper.setProps({
+      searchText: input,
+      isFocused: true
+    })
+    assert.equal(wrapper.find(Preloader).length, 1)
+  })
+  it('should show ErrorMsg', function() {
+    var input = 'foo'
+    var mock = sinon.stub(searchIngredients).throws("TyperError")
+    var wrapper = mount(<SuggestionsList />)
+    wrapper.setProps({
+      searchText: input,
+      isFocused: true
+    })
+    function assertTest() {
+      mock.restore()
+      assert.equal(wrapper.find(ErrorMsg).length, 1)
+    }
+    setTimeout(assertTest, 10)
+  })
+})
+describe('IngredientSuggestion', function() {
+  it('should show title', function() {
+    var wrapper = shallow(<IngredientSuggestion />)
+    wrapper.setProps({
+      title: 'foo'
+    })
+    assert.equal(wrapper.contains(<h4 className='media-heading'>foo</h4>),true)
+  })
+  it('should show the image', function() {
+    var wrapper = shallow(<IngredientSuggestion />)
+    wrapper.setProps({
+      src: 'foo',
+    })
+    assert.equal(wrapper.contains(<img className='img-rounded' src='foo' alt='50x48' width='50' height='50'/>),true)
   })
 })
--- test/apicalls-test.js
+++ test/apicalls-test.js
@@ -1,35 +1,22 @@
-var simple = require('simple-mock')
+var sinon = require('sinon')
 var assert = require('assert')
 var searchIngredients = require('../app/models/apicalls')
 var request = require('request')
 var events = require('events');
 
 describe('searchIngredients()', function() {
-  afterEach(function () {
-  simple.restore()
-})
   it('should call get', function() {
-    simple.mock(searchIngredients, 'get')
+    var mock = sinon.spy(searchIngredients.get)
     searchIngredients('foo', true, function(err, res, body) {
       if (err) return done(err)
 
-      assert.equal(apicalls.get.called, true)
+      assert.equal(mock.calledOnce, true)
     })
   })
-  it('should return data', function() {
-    simple.mock(request, 'get').callbackWith(null, 200, 'foo')
-
-    searchIngredients('foo', true, function(err, res, body) {
-      if (err) return done(err)
-
-      assert.equal(body, 'foo')
-    })
-  })
-  it('should catch error', function() {
-    simple.mock(request, 'get').callbackWith('error', 200, 'foo')
-
-    searchIngredients('foo', true, function(err, res, body) {
-      assert.equal(err, 'error')
-    })
+  it('should call request.get function', function() {
+    var mock = sinon.stub(request, 'get')
+    searchIngredients('foo', function(err, res, body) { })
+    mock.restore()
+    sinon.assert.calledOnce(mock)
   })
 })
