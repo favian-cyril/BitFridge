f3b97b1e4a4e16fc62b90426f1421779894332f2
chadmadna <chadmadna@gmail.com>
2016-10-06 12:32:15 +0700

    Fixed cards-test stubs

--- test/cards-test.js
+++ test/cards-test.js
@@ -7,15 +7,15 @@ import jsdom from 'jsdom'
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
 global.document = doc
 global.window = doc.defaultView
-describe('card', function()) {
-  it ('should have a title', function() {
+describe('card', function() {
+  it('should have a title', function() {
 
     })
-  it ("should change the state when setting is called", function(){
+  it("should change the state when setting is called", function(){
 
   })
-  it ("should show the contents", function(){
+  it("should show the contents", function(){
 
   })
 
-}
+})
