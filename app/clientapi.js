36040f46d62c8398d253932e0591e91a17ef6307
chadmadna <chadmadna@gmail.com>
2016-10-08 02:19:32 +0700

    Created client-side API requests for adding ingredients

--- app/clientapi.js
+++ app/clientapi.js
@@ -14,11 +14,26 @@ function searchIngredients (string, cb) {
   get(url, params, cb)
 }
 
+function addIngredient(ingredient, cb) {
+  var url = baseUrl + 'fridge/add'
+  var form = { item: ingredient }
+  post(url, form, cb)
+}
+
 function get (url, params, cb) {
   url = append(url, params)
   request.get(url, function (err, res, body) {
     if (!err && res.statusCode == 200)
       cb(null, res, JSON.parse(body))
+  }).on('error', function (err) {
+    cb(err)
+  })
+}
+
+function post (url, obj, cb) {
+  request.post({ url: url, form: obj }, function (err, res, body) {
+    if (!err && res.statusCode == 200)
+      cb(null, res)
   }).on('error', function (err) {
     cb(err)
   })
