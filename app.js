b3471b20e0824bf1b5c81c8bad7b75d8fe62f898
chadmadna <chadmadna@gmail.com>
2016-10-08 18:43:51 +0700

    Tidied up app.js and setup Redis session store

--- app.js
+++ app.js
@@ -1,38 +1,56 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
 
-var routes = require('./routes/index');
-var api = require('./routes/api');
-var users = require('./routes/users');
+// express utility middleware
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var session = require('express-session')
+// var crypto = require('crypto')
+var RedisStore = require('connect-redis')(session)
 
-var app = express();
+// routes middleware
+var routes = require('./routes/index')
+var api = require('./routes/api')
+
+// initialize app
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', routes);
-app.use('/api', api);
-app.use('/users', users);
+// session store setup
+// var secretKey = crypto.randomBytes(64).toString('hex')
+app.use(session({
+  store: new RedisStore({
+    host: 'localhost',   // DEVELOPMENT ONLY
+    port: 6379
+  }),
+  secret: 'this is a not-so-secret key',
+  saveUninitialized: false,
+  resave: false
+}))
+
+// router middleware
+app.use('/', routes)
+app.use('/api', api)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handlers
 
@@ -40,23 +58,23 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+    res.status(err.status || 500)
     res.render('error', {
       message: err.message,
       error: err
-    });
-  });
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
 
-module.exports = app;
+module.exports = app
