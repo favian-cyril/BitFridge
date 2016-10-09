77c4437d3be6a6a849914f2f4d722dd278ef404d
chadmadna <chadmadna@gmail.com>
2016-10-09 13:01:38 +0700

    Implemented CardContainer, changed its tests

--- app/containers/CardContainer.js
+++ app/containers/CardContainer.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import CardContent from '../components/CardContent'
 
 export default class CardContainer extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      settings: this.props.settings,
+      contents: this.props.contents
+    }
   }
   render() {
-    return null
+    return (
+      <div className={'container ' + this.props.classname}>
+        <CardContent contents={this.props.contents}/>
+      </div>
+    )
   }
+}
+
+CardContainer.defaultProps = {
+  settings: {},
+  contents: [],
+  title: 'Default Title'
 }
--- test/cards-test.js
+++ test/cards-test.js
@@ -3,8 +3,8 @@ import {mount, shallow} from 'enzyme'
 import {assert} from 'chai'
 import React from 'react'
 import CardContainer from '../app/containers/CardContainer'
-import CardContent from '../app/components/SuggestionsList'
-import IngredientList from '../app/components/IngredientList'
+import CardContent from '../app/components/CardContent'
+import IngredientSuggestion from '../app/components/IngredientSuggestion'
 
 import jsdom from 'jsdom'
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
@@ -13,25 +13,26 @@ global.window = doc.defaultView
 describe('CardContainer', function() {
   it('should have a title', function() {
     var input = 'foo'
-    var wrapper = mount(<CardContainer title={input} settings={null} />)
-    assert.equal(input,wrapper.state('title'))
+    var wrapper = mount(<CardContainer title={input}/>)
+    assert.equal(input, wrapper.instance().props.title)
     })
   it("should have settings", function(){
-    var settings = new Object()
-    var wrapper = mount(<CardContainer title='foo' settings={settings} />)
-    assert.equal(true,wrapper.state('settings') instanceof Object)
+    var settings = {}
+    var wrapper = mount(<CardContainer settings={settings}/>)
+    assert.equal(true, wrapper.state('settings') instanceof Object)
   })
   it("should have content", function(){
-    var wrapper = shallow(<CardContainer />)
+    var wrapper = shallow(<CardContainer contents={<CardContent />}/>)
     assert.equal(wrapper.find(CardContent).length, 1)
   })
 })
-describe('CardContent' function() {
+describe('CardContent', function() {
   it('should show list of items', function() {
-    var wrapper = mount(<CardContent />)
-    wrapper.setState({
-      results: [('foo', 'bar'), ('foo', 'bar')]
-    })
-    assert.equal(wrapper.find(IngredientList).length, 2)
+    var items = [
+      {name: 'foo', image: 'bar'},
+      {name: 'bar', image: 'foo'}
+    ]
+    var wrapper = mount(<CardContent contents={items}/>)
+    assert.equal(wrapper.find(IngredientSuggestion).length, 2)
   })
 })
