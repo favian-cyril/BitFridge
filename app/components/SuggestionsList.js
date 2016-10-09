94c3a94e958d7b13dc68522fd771b47a18dc35d7
chadmadna <chadmadna@gmail.com>
2016-10-09 10:50:04 +0700

    Fixed state.results initialization in SuggestionsList

--- app/components/SuggestionsList.js
+++ app/components/SuggestionsList.js
@@ -9,7 +9,7 @@ export default class SuggestionsList extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      results: '',
+      results: [],
       loading: false,
       timestamp: null,
       errtype: null
