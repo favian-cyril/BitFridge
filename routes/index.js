13177897d09490b450f3a9a4307f35d7036b0489
chadmadna <chadmadna@gmail.com>
2016-10-18 16:35:14 +0700

    Injected baseurl data attr for client to detect environment and recognize host

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,13 @@ router.get('/', function(req, res, next) {
   if (!req.session.key) {
     req.session.key = uuid.v1()
   }
-  res.render('index', { title: 'BitFridge', _csrfToken: req.csrfToken() })
+  var baseurl
+  if (process.env.NODE_ENV == 'production') {
+    baseurl = 'http://188.166.247.122/api'
+  } else {
+    baseurl = 'http://localhost:3000/api'
+  }
+  res.render('index', { title: 'BitFridge', _csrfToken: req.csrfToken(), baseurl: baseurl })
 })
 
 module.exports = router
