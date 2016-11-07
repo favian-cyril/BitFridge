42c4da69d02738d4757f8fe19a2fcc71020f2eb6
favial-cyril <favian.cyril@gmail.com>
2016-11-07 18:49:42 +0700

    Finished RecipeComponent-test and serverApicall

--- test/apicalls-test.js
+++ test/apicalls-test.js
@@ -1,13 +1,42 @@
 import sinon from 'sinon'
 import assert from 'assert'
-import {searchIngredients, get}  from '../modules/apicalls'
+import {searchIngredients, searchResults}  from '../modules/apicalls'
 import axios from 'axios'
+import sinonStubPromise from 'sinon-stub-promise'
 
-describe('server searchIngredients()', function() {
-  it('should call get', function() {
+sinonStubPromise(sinon)
 
+describe('server apicalls', function() {
+  describe('searchIngredients', function() {
+    it('should return with answer when parsed text', sinon.test(function() {
+      var res = {data:'bar'}
+      var input = 'foo'
+      var mock = sinon.stub(axios, 'get').returnsPromise().resolves(res)
+      searchIngredients(input, 5, function(err, body) {
+        assert.equal(body,res.data)
+        mock.restore()
+      })
+    }))
+    it('should return with error when something happens', sinon.test(function() {
+      var err = 'fooError'
+      var input = 'foo'
+      var mock = sinon.stub(axios, 'get').returnsPromise().rejects(err)
+      searchIngredients(input, 5, function(error, body) {
+        assert.equal(error, err)
+        mock.restore()
+      })
+    }))
   })
-  it('should call axios.get function',  sinon.test(function() {
-
-  }))
+  describe('searchResults', function() {
+    it('should return with answer when parsed an array of ingredients', sinon.test(function() {
+      var input = ['foo','bar','baz']
+      var res = {data:['foo','bar','baz']}
+      var linkArr = ['link','link','link']
+      var mock1 = sinon.stub(axios, 'get').returnsPromise().resolves(res)
+      searchResults(input, 1, function(err, body) {
+        assert.equal(mock1.callCount, res.data.length+1)
+        mock1.restore()
+      })
+    }))
+  })
 })
