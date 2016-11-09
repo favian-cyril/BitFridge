8c3c17df83a2a0d939c1921871f77cea1659dd86
chadmadna <chadmadna@gmail.com>
2016-11-09 17:20:35 +0700

    Server minor changes

--- app.js
+++ app.js
@@ -134,9 +134,9 @@ app.use(function (req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function (err, req, res, next) {
     res.status(err.status || 500)
-    res.json({
+    res.render('error', {
       message: err.message,
-      stack: err.stack
+      error: err
     })
   })
 }
