9db54efd6a929e0ffbdf3988a6f03b80996e9916
chadmadna <chadmadna@gmail.com>
2016-11-22 12:28:23 +0700

    Annotated MainContainer

---
+++ stateTree.js
@@ -0,0 +1,24 @@
+const state = {
+  ready: false,
+  display: null,
+  searchBar: {
+    searchText: '',
+    isFocused: false,
+    isLoading: false,
+    timestamp: null,
+    errorType: '',
+    suggestionResults: []
+  },
+  fridge: {
+    contents: [],
+    isLoading: false,
+    errorType: ''
+  },
+  recipes: {
+    contents: [],
+    page: 1,
+    isLoading: false,
+    errorType: ''
+  },
+  userData: null
+}
