bc123e17f6c360de3e96e871aab0891835c64231
chadmadna <chadmadna@gmail.com>
2016-11-25 15:03:31 +0700

    [WIP] Implemented App as MainContainer replacement

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { browserHistory } from 'react-router'
+import Preloader from '../components/Preloader'
 import * as actions from '../actions'
+import { REDIRECT_INGR_THRESHOLD } from '../config/constants'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -9,10 +12,60 @@ export default class App extends React.Component {
   }
   
   componentDidMount() {
-    const { dispatch, location } = this.props
+    const { dispatch, fridge, location } = this.props
     dispatch(actions.fetchUserData())
       .then(() => {
-
+        dispatch(actions.setDisplay(location.pathname))
+        Promise.resolves(fridge.contents.length > 0)
+          .then((shouldFetchRecipes) => {
+            if (shouldFetchRecipes) {
+              dispatch(actions.fetchRecipes())
+            }
+          })
+          .then(() => {
+            dispatch(actions.setReady())
+          })
       })
   }
+
+  componentWillUpdate(nextProps) {
+    if (this.props.fridge.contents.length !== nextProps.fridge.contents.length) {
+      const { dispatch, fridge } = nextProps
+      // Determine which path the router should transition into and which display should be set
+      const onThreshold = this.props.fridge.contents.length === REDIRECT_INGR_THRESHOLD
+      const shouldTransitionToIndex = fridge.length < REDIRECT_INGR_THRESHOLD && onThreshold
+      const shouldTransitionToDash = fridge.length > REDIRECT_INGR_THRESHOLD && onThreshold
+      const pathname = shouldTransitionToIndex ? '/' : shouldTransitionToDash ? '/dash' : null
+      // Make router transition then set appropriate display
+      Promise.resolves(pathname)
+        .then((pathname) => {
+          browserHistory.push(pathname)
+        })
+        .then(() => {
+          dispatch(actions.setDisplay(pathname))
+        })
+    }
+  }
+
+  render() {
+    return (
+      <div className="main-container">
+        {
+          this.state.ready
+            ? React.cloneElement(this.props.children, {
+            // props
+            })
+            : <div className="absolute-center"><Preloader/></div>
+        }
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+
+}
+
+App.childContextTypes = {
+
 }
