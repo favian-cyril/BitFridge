8670cc7bf951cfeb23773f67ae31a3683881154d
favian-cyril <favian.cyril@gmail.com>
2016-12-10 20:43:09 +0700

    Rebased against master

--- app/components/Fridge.js
+++ app/components/Fridge.js
@@ -4,32 +4,69 @@ import Ingredient from '../components/Ingredient'
 
 const Fridge = props => (
   <div className="card">
-    <div className="card-block">
-      <h5 className="card-title">
-        <span className="bf bf-fridge"></span>
-        {props.title}
-      </h5>
-    </div>
-    <div className="fridge-content list-wrapper">
-      <ul className="media-list">
-        <TransitionGroup
-          transitionName="fridge-contents"
-          transitionEnterTimeout={600}
-          transitionLeaveTimeout={600}
-        >
-          {
-            props.contents.map((item, i) => (
-              <Ingredient
-                key={i}
-                ingredient={item}
-                idName={`fridge-${i}`}
-                parent={'fridge'}
-              />
-              )
-            )
-          }
-        </TransitionGroup>
+    <div className="card-block frg-shp">
+      <ul className="nav nav-tabs" id="fridge-shop" role="tablist">
+        <li className="nav-item">
+          <a className="nav-link active" data-toggle="tab" href="#fridge" role="tab">
+            <span className="bf bf-fridge"></span>
+            Fridge
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" data-toggle="tab" href="#shopping-list" role="tab">
+            <span className="bf bf-shopping-basket"></span>
+            Shopping List
+          </a>
+        </li>
       </ul>
+    </div>
+    <div className="tab-content">
+      <div className="tab-pane fade in active" id="fridge" role="tabpanel">
+        <div className="list-wrapper">
+          <ul className="media-list">
+            <TransitionGroup
+              transitionName="fridge-contents"
+              transitionEnterTimeout={600}
+              transitionLeaveTimeout={600}
+            >
+              {
+                props.contents.map((item, i) => (
+                    <Ingredient
+                      key={i}
+                      ingredient={item}
+                      idName={`fridge-${i}`}
+                      parent={'fridge'}
+                    />
+                  )
+                )
+              }
+            </TransitionGroup>
+          </ul>
+        </div>
+      </div>
+      <div className="tab-pane fade" id="shopping-list" role="tabpanel">
+        <div className="list-wrapper">
+          <ul className="media-list">
+            <TransitionGroup
+              transitionName="fridge-contents"
+              transitionEnterTimeout={600}
+              transitionLeaveTimeout={600}
+            >
+              {
+                props.contents.map((item, i) => (
+                    <Ingredient
+                      key={i}
+                      ingredient={item}
+                      idName={`shopping-list-${i}`}
+                      parent={'shopping-list'}
+                    />
+                  )
+                )
+              }
+            </TransitionGroup>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 )
